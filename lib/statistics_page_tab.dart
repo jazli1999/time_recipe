60f4051ff96288c38dec953ed7618d21bbb2ab8d
Jasmine Li <jasli1999@outlook.com>
2021-02-21 19:45:58 +0800

    feat: basic info on cart page

--- lib/statistics_page_tab.dart
+++ lib/statistics_page_tab.dart
@@ -1,22 +1,187 @@
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'styles.dart';
+
+import 'model/app_state_model.dart';
+
+const double _kDateTimePickerHeight = 216;
 
 class StatisticsPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return _StatisticsPageState();
-    }
+  _StatisticsPageState createState() {
+    return _StatisticsPageState();
+  }
 }
 
 class _StatisticsPageState extends State<StatisticsPage> {
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: const <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Statistics'),
+  String name;
+  String email;
+  String location;
+  String pin;
+  DateTime dateTime = DateTime.now();
+
+  Widget _buildNameField() {
+    return CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.person_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Name',
+      onChanged: (newName) {
+        setState(() {
+          name = newName;
+        });
+      },
+    );
+  }
+
+  Widget _buildEmailField() {
+    return const CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.mail_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Email',
+    );
+  }
+
+  Widget _buildLocationField() {
+    return const CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.location_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Location',
+    );
+  }
+
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
+      AppStateModel model) {
+    return SliverChildBuilderDelegate((context, index) {
+      switch (index) {
+        case 0:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: _buildNameField(),
+          );
+        case 1:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: _buildEmailField(),
+          );
+        case 2:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: _buildLocationField(),
+          );
+        case 3:
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
+            child: _buildDateAndTimePicker(context),
+          );
+        default:
+      }
+      return null;
+    });
+  }
+
+  Widget _buildDateAndTimePicker(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const <Widget>[
+                Icon(
+                  CupertinoIcons.clock,
+                  color: CupertinoColors.lightBackgroundGray,
+                  size: 28,
+                ),
+                SizedBox(width: 6),
+                Text(
+                  'Delivery time',
+                  style: Styles.deliveryTimeLabel,
+                ),
+              ],
+            ),
+            Text(
+              DateFormat.yMMMd().add_jm().format(dateTime),
+              style: Styles.deliveryTime,
+            ),
+          ],
+        ),
+        Container(
+          height: _kDateTimePickerHeight,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.dateAndTime,
+            initialDateTime: dateTime,
+            onDateTimeChanged: (newDateTime) {
+              setState(() {
+                dateTime = newDateTime;
+              });
+            },
+          ),
         ),
       ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateModel>(builder: (context, model, child) {
+      return CustomScrollView(slivers: <Widget>[
+        const CupertinoSliverNavigationBar(
+          largeTitle: Text('Statistics'),
+        ),
+        SliverSafeArea(
+            top: false,
+            minimum: const EdgeInsets.only(top: 4),
+            sliver: SliverList(
+              delegate: _buildSliverChildBuilderDelegate(model),
+            ))
+      ]);
+    });
   }
 }
